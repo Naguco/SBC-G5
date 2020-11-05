fe1aca6438c86530f861ec536d0887654a0c6d13
Naguco <naguco@gmail.com>
2020-11-05 14:50:38 +0100

    Ficheros separados

---
+++ main/wifi.ino
@@ -0,0 +1,25 @@
+const char* ssid = "FranPrueba";
+const char* password = "sbc$2020";
+
+void wifiSetup() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+}
