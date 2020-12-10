b7c5a291f135afdedd118ee6b584712e373fd8a0
Naguco <naguco@gmail.com>
2020-12-10 15:36:59 +0100

    OTA Aplicada correctamente

--- main/main.ino
+++ main/main.ino
@@ -4,11 +4,14 @@
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
 #include <DHT.h>
+#include <HTTPClient.h>
+#include <HTTPUpdate.h>
 
 #define TOKEN "5Rdt2HPlKGz6bBQjc5Fc"
 #define CLIENTID "d76a30a0-24ea-11eb-b0e1-d73cf2f8386f"
 #define DHTPIN 32
 #define DHTTYPE DHT22
+#define versionActual 0
 
 // Declaraciones variables globales.
 const char* serverName = "https://demo.thingsboard.io/api/v1/RLGdJssAKeG0PwnExDgr/telemetry";
@@ -18,6 +21,7 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 char data[] = "Hola";
 DHT dht(DHTPIN, DHTTYPE);
+char checkedVersion = 0;
 
 // Declaraciones funciones globales.
 void wifiSetup();
@@ -34,12 +38,14 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Booting");
   wifiSetup();
-  otaSetup();
-  mqttSetup();
-  initDHT();
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  delay(5000);
+  otaSetup();
+  delay(5000);
+  mqttSetup();
+  initDHT();
 }
 
 void loop() {
@@ -48,8 +54,6 @@ void loop() {
     mqttReconnect();
   }
   client.loop();
-  //float temp = map(analogRead(33), 4095, 1, -60, 150);
-  //Serial.println(analogRead(33));
   publishData("Moisture",readMoisture());
   publishData("Temperatura", (int) leerTemperatura());
   publishData("Humedad", (int) leerHumedad());
